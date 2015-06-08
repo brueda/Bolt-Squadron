678470e8ebf0c75006650c4e36649c01acb7eab6
Ben Rueda <bjrueda@buffalo.edu>
2015-06-07 23:18:09 -0400

    forgot to add new files

---
+++ app/src/main/java/com/Ben/game/classes/PlayerShip.java
@@ -0,0 +1,10 @@
+package com.Ben.game.classes;
+
+/**
+ * Created by Benjamin on 6/7/2015.
+ */
+public abstract class PlayerShip extends Ship {
+    public PlayerShip(){
+        super();
+    }
+}
