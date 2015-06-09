49c3ca3ae971045d9a16ca678c57211fe3cb71ae
Thomas Mitchell <klaue@live.com>
2015-06-09 12:27:12 -0400

    Task and Tasklist are being added to the repository.

---
+++ app/src/main/java/com/Ben/framework/util/Task.java
@@ -0,0 +1,54 @@
+package com.Ben.framework.util;
+
+import com.Ben.simpleandroidgdf.BuildConfig;
+
+/**
+ * Created by homedesk on 6/8/2015.
+ */
+public abstract class Task {
+
+    public static final int TASK_NEW = 0;
+    public static final int TASK_READY = 1;
+    public static final int TASK_DONE = 2;
+
+    private int _state;
+    private Task _child;
+    private TaskList _owner;
+
+    public Task(TaskList owner) {
+        _owner = owner;
+        _state = TASK_NEW;
+        _child = null;
+    }
+
+    //Most usually called from within a specific task's initialization function.
+    //The initialization function will not be made abstract as a part of the interface,
+    //  because the signature should probably be left up to the specific task.
+    public void makeRunnable()
+    {
+        if (!(_state == TASK_NEW)) { throw new AssertionError("Task was already made runnable."); }
+        _state = TASK_READY;
+    }
+
+    //If the task has a child task, then that task will be promoted up.
+    public void finishTask()
+    {
+        if (!(_state == TASK_READY)) { throw new AssertionError("Task was not running."); }
+        if(_child != null) {
+            _owner.addTask(_child);
+            _child.makeRunnable();
+        }
+        _state = TASK_DONE;
+    }
+
+    //TODO: This must be overridden. This is what makes a specific instance of a Task unique.
+    public abstract void update(long deltaTime);
+
+    public void attachChild(Task child) { _child = child; }
+
+    public void detachChild() { _child = null; }
+
+    public int getState() {
+        return _state;
+    }
+}
---
+++ app/src/main/java/com/Ben/framework/util/TaskList.java
@@ -0,0 +1,40 @@
+package com.Ben.framework.util;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Klaue on 6/8/2015.
+ */
+public class TaskList {
+
+    private static ArrayList<Task> _list;
+    private static ArrayList<Task> _toAdd;
+    private static ArrayList<Task> _toDelete;
+
+    public TaskList() {
+        _list = new ArrayList<Task>();
+        _toAdd = new ArrayList<Task>();
+        _toDelete = new ArrayList<Task>();
+    }
+
+    //Adds a previously created task to the tasklist.
+    public static void addTask(Task t) {
+        _toAdd.add(t);
+    }
+
+    public static  void updateAll(long deltaTime) {
+        for (Task t : _list) {
+            if (t.getState() == Task.TASK_READY) { t.update(deltaTime); }
+            if (t.getState() == Task.TASK_DONE) { _toDelete.add(t); }
+        }
+        cleanup();
+    }
+
+    private static void cleanup() {
+        for (Task t : _toDelete) { _list.remove(t); }
+        for (Task t : _toAdd) { _list.add(t); }
+        _toDelete.clear();
+        _toAdd.clear();
+    }
+
+}
